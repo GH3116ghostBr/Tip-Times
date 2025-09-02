e7e4ab682e10862d997f46a012ffca76bc488fdc
= <=>
2025-09-01 22:33:06 -0300

    Criei o projeto TipTime - 01/09/2025 - 22:30h

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void ArredondaCimaBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+        }
+
+        private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;            
+        }
+
+        private void ArredondaBaixoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double porcentagem = PorcentagemSlider.Value;
+            double gorjeta;
+            double valortotal = float.Parse(ValorTotalEntry.Text);
+            float valorfinal;
+            gorjeta = valortotal * (porcentagem / 100);
+            ValorGorjetaLabel.Text = $"R$ {gorjeta}";
+
+        }
+    }
+
+}
